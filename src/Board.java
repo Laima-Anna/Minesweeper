42444a71d534850a23583c7911561f0dd2f44cb7
Eva Ibrus <eva.ibrus@hotmail.com>
2019-04-03 23:28:01 +0300

    Made improvements to getNumbersAround method

--- src/Board.java
+++ src/Board.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Board {
     int boardWidth;
     int boardHeight;
     int[][] board;
-    private ArrayList<ArrayList> bombCoordinates;
+    private List<ArrayList> bombCoordinates;
 
     public Board(int boardHeight, int boardWidth) {
         this.boardWidth = boardWidth;
@@ -28,13 +29,11 @@ public class Board {
         return board;
     }
 
-
-
-    ArrayList<ArrayList> setBombCoordinates(int bombCount) {
+    List<ArrayList> setBombCoordinates(int bombCount) {
 
         for (int i = 0; i < bombCount; i++) {
-            Bombs bomb = new Bombs(boardHeight,boardWidth);
-            if(!bombCoordinates.contains(bomb.getLocation()))
+            Bombs bomb = new Bombs(boardHeight, boardWidth);
+            if (!bombCoordinates.contains(bomb.getLocation()))
                 bombCoordinates.add(bomb.getLocation());
             else {
                 return setBombCoordinates(bombCount - i);
@@ -43,10 +42,7 @@ public class Board {
         return bombCoordinates;
     }
 
-
-
-
-    void setBomb(ArrayList<ArrayList> coordinates){
+    void setBomb(List<ArrayList> coordinates) {
         for (ArrayList coordinate : coordinates) {
             for (int j = 0; j < boardHeight; j++) {
                 for (int k = 0; k < boardWidth; k++) {
@@ -59,115 +55,49 @@ public class Board {
         }
     }
 
-    int countOpened(){
-        int count=0;
+    int countOpened() {
+        int count = 0;
         for (int i = 0; i < boardHeight; i++) {
             for (int j = 0; j < boardWidth; j++) {
-                if(board[i][j]>=0)count+=1;
+                if (board[i][j] >= 0) count += 1;
             }
         }
         return count;
     }
 
-    int[] getNumbersAround(int x, int y){ //TODO better way of doing? --Eva
+    int[] getNumbersAround(int x, int y) {
         int[] numbers = new int[8];
-        if(x==0&&y==0){
-            numbers[0] = -9;
-            numbers[1] = -9;
-            numbers[2] = -9;
-            numbers[3] = board[x][y + 1];
-            numbers[4] = board[x + 1][y + 1];
-            numbers[5] = board[x + 1][y];
-            numbers[6] = -9;
-            numbers[7] = -9;
-        } else if(x==0&&y==boardWidth-1){
-            numbers[0] = -9;
-            numbers[1] = -9;
-            numbers[2] = -9;
-            numbers[3] = -9;
-            numbers[4] = -9;
-            numbers[5] = board[x + 1][y];
-            numbers[6] = board[x + 1][y - 1];
-            numbers[7] = board[x][y - 1];
-        } else if (x == boardHeight - 1 && y == 0) {
-            numbers[0] = -9;
-            numbers[1] = board[x - 1][y];
-            numbers[2] = board[x - 1][y + 1];
-            numbers[3] = board[x][y + 1];
-            numbers[4] = -9;
-            numbers[5] = -9;
-            numbers[6] = -9;
-            numbers[7] = -9;
-        } else if (x == boardHeight - 1 && y == boardWidth - 1) {
-            numbers[0] = board[x - 1][y - 1];
-            numbers[1] = board[x-1][y];
-            numbers[2] = -9;
-            numbers[3] = -9;
-            numbers[4] = -9;
-            numbers[5] = -9;
-            numbers[6] = -9;
-            numbers[7] = board[x][y - 1];
-        } else if (x == 0) {
-            numbers[0] = -9;
-            numbers[1] = -9;
-            numbers[2] = -9;
-            numbers[3] = board[x][y + 1];
-            numbers[4] = board[x + 1][y + 1];
-            numbers[5] = board[x + 1][y];
-            numbers[6] = board[x + 1][y - 1];
-            numbers[7] = board[x][y - 1];
-        } else if (y == 0) {
-            numbers[0] = -9;
-            numbers[1] = board[x - 1][y];
-            numbers[2] = board[x - 1][y + 1];
-            numbers[3] = board[x][y + 1];
-            numbers[4] = board[x + 1][y + 1];
-            numbers[5] = board[x + 1][y];
-            numbers[6] = -9;
-            numbers[7] = -9;
-        } else if (x == boardHeight - 1) {
-            numbers[0] = board[x - 1][y - 1];
-            numbers[1] = board[x - 1][y];
-            numbers[2] = board[x - 1][y + 1];
-            numbers[3] = board[x][y + 1];
-            numbers[4] = -9;
-            numbers[5] = -9;
-            numbers[6] = -9;
-            numbers[7] = board[x][y - 1];
-        } else if (y == boardWidth - 1) {
-            numbers[0] = board[x - 1][y - 1];
-            numbers[1] = board[x - 1][y];
-            numbers[2] = -9;
-            numbers[3] = -9;
-            numbers[4] = -9;
-            numbers[5] = board[x + 1][y];
-            numbers[6] = board[x + 1][y - 1];
-            numbers[7] = board[x][y - 1];
-        } else {
-            numbers[0] = board[x - 1][y - 1];
-            numbers[1] = board[x - 1][y];
-            numbers[2] = board[x - 1][y + 1];
-            numbers[3] = board[x][y + 1];
-            numbers[4] = board[x + 1][y + 1];
-            numbers[5] = board[x + 1][y];
-            numbers[6] = board[x + 1][y - 1];
-            numbers[7] = board[x][y - 1];
-        }
 
+        int a = 0;
+
+        for (int row = x - 1; row < x + 2; row++) {
+            for (int col = y - 1; col < y + 2; col++) {
+                if (!(row ==x && col == y)) {
+                    if (checkIndexNotOutOfBounds(row, col, boardHeight, boardWidth)) {
+                        numbers[a] = board[row][col];
+
+                    //if index is out of bounds, add -9 to the array
+                    } else {
+                        numbers[a] = -9;
+                    }
+                    a++;
+                }
+            }
+        }
         return numbers;
     }
 
-    boolean checkIndexOutOfBounds(int i, int j, int height, int width) {
+    private boolean checkIndexNotOutOfBounds(int i, int j, int height, int width) {
         if (i < 0 || j < 0 || i >= height || j >= width)
             return false;
         return true;
     }
 
-    int countBombs(int i, int j) {
+    private int countBombs(int i, int j) {
         int counter = 0;
-        for (int row = i-1; row < i+2; row++) {
-            for (int col = j-1; col < j+2; col++) {
-                if (checkIndexOutOfBounds(row, col, boardHeight, boardWidth) && board[row][col] == -1) {
+        for (int row = i - 1; row < i + 2; row++) {
+            for (int col = j - 1; col < j + 2; col++) {
+                if (checkIndexNotOutOfBounds(row, col, boardHeight, boardWidth) && board[row][col] == -1) {
                     counter++;
                 }
             }
@@ -175,11 +105,11 @@ public class Board {
         return counter;
     }
 
-    void setNumbers(){
+    void setNumbers() {
 
         for (int i = 0; i < boardHeight; i++) {
             for (int j = 0; j < boardWidth; j++) {
-                if(board[i][j]!=-1) {
+                if (board[i][j] != -1) {
                     board[i][j] = countBombs(i, j);
                 }
             }
@@ -190,7 +120,7 @@ public class Board {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < boardHeight; i++) {
             for (int j = 0; j < boardWidth; j++) {
-                sb.append(String.format("%1$"+3+ "s", board[i][j]));
+                sb.append(String.format("%1$" + 3 + "s", board[i][j]));
                 sb.append(" ");
             }
             sb.append("\n");
--- src/MainClass.java
+++ src/MainClass.java
@@ -14,9 +14,9 @@ public class MainClass {
         List<List<Integer>> checked = new ArrayList<>();
         int bombsRightNow = 0;
         int openedSquaresNow = 0;
-        int allFreeSpaces = boardHeight*boardWidth-bombCount;
+        int allFreeSpaces = boardHeight * boardWidth - bombCount;
 
-        ArrayList<ArrayList> coordinates = realBoard.setBombCoordinates(bombCount);
+        List<ArrayList> coordinates = realBoard.setBombCoordinates(bombCount);
 
         realBoard.setBomb(coordinates);
         realBoard.setNumbers();
@@ -55,10 +55,10 @@ public class MainClass {
                 } else if (realBoard.getBoard()[x][y] == 0) {
                     openAround(x, y, userBoard, realBoard, checked);
                 }
-                openedSquaresNow=userBoard.countOpened(); //TODO check
+                openedSquaresNow = userBoard.countOpened(); //TODO check
             } else System.out.println("Wrong input");
 
-            if (openedSquaresNow==allFreeSpaces) { //if there is the same number of flags as originally planned
+            if (openedSquaresNow == allFreeSpaces) { //if there is the same number of flags as originally planned
                 System.out.println("You won!");
                 break;
             }
@@ -66,66 +66,67 @@ public class MainClass {
             M_abi.printM_ind(" ", realBoard.getBoard()); //TODO better way? Showing author?
             System.out.println();
             M_abi.printM_ind(" ", userBoard.getBoard());
-
-
         }
-
     }
 
+
     public static void openAround(int x, int y, UserBoard board, Board realBoard, List<List<Integer>> checked) {
-    //TODO should we leave it here?
+        //TODO should we leave it here?
         int[] numbers = realBoard.getNumbersAround(x, y);
-        //System.out.println(Arrays.toString(numbers));
 
         List<Integer> uus = new ArrayList<>();
         uus.add(x);
         uus.add(y);
         if (!checked.contains(uus)) checked.add(uus);
 
-
         if (numbers[0] != -9)
             board.getBoard()[x - 1][y - 1] = realBoard.getBoard()[x - 1][y - 1]; //TODO can be a function
         if (numbers[1] != -9) board.getBoard()[x - 1][y] = realBoard.getBoard()[x - 1][y];
         if (numbers[2] != -9) board.getBoard()[x - 1][y + 1] = realBoard.getBoard()[x - 1][y + 1];
-        if (numbers[3] != -9) board.getBoard()[x][y + 1] = realBoard.getBoard()[x][y + 1];
-        if (numbers[4] != -9) board.getBoard()[x + 1][y + 1] = realBoard.getBoard()[x + 1][y + 1];
-        if (numbers[5] != -9) board.getBoard()[x + 1][y] = realBoard.getBoard()[x + 1][y];
-        if (numbers[6] != -9) board.getBoard()[x + 1][y - 1] = realBoard.getBoard()[x + 1][y - 1];
-        if (numbers[7] != -9) board.getBoard()[x][y - 1] = realBoard.getBoard()[x][y - 1];
+        if (numbers[3] != -9) board.getBoard()[x][y - 1] = realBoard.getBoard()[x][y - 1];
+        if (numbers[4] != -9) board.getBoard()[x][y + 1] = realBoard.getBoard()[x][y + 1];
+        if (numbers[5] != -9) board.getBoard()[x + 1][y - 1] = realBoard.getBoard()[x + 1][y - 1];
+        if (numbers[6] != -9) board.getBoard()[x + 1][y] = realBoard.getBoard()[x + 1][y];
+        if (numbers[7] != -9) board.getBoard()[x + 1][y + 1] = realBoard.getBoard()[x + 1][y + 1];
 
         List<Integer> uus2 = new ArrayList<>();
         uus2.add(x - 1);
         uus2.add(y - 1);
         if (numbers[0] == 0 && !checked.contains(uus2)) openAround(x - 1, y - 1, board, realBoard, checked);
+
         List<Integer> uus3 = new ArrayList<>();
         uus3.add(x - 1);
         uus3.add(y);
         if (numbers[1] == 0 && !checked.contains(uus3)) openAround(x - 1, y, board, realBoard, checked);
+
         List<Integer> uus4 = new ArrayList<>();
         uus4.add(x - 1);
         uus4.add(y + 1);
         if (numbers[2] == 0 && !checked.contains(uus4)) openAround(x - 1, y + 1, board, realBoard, checked);
+
         List<Integer> uus5 = new ArrayList<>();
         uus5.add(x);
-        uus5.add(y + 1);
-        if (numbers[3] == 0 && !checked.contains(uus5)) openAround(x, y + 1, board, realBoard, checked);
+        uus5.add(y - 1);
+        if (numbers[3] == 0 && !checked.contains(uus5)) openAround(x, y - 1, board, realBoard, checked);
+
         List<Integer> uus6 = new ArrayList<>();
-        uus6.add(x + 1);
+        uus6.add(x);
         uus6.add(y + 1);
-        if (numbers[4] == 0 && !checked.contains(uus6)) openAround(x + 1, y + 1, board, realBoard, checked);
+        if (numbers[4] == 0 && !checked.contains(uus6)) openAround(x, y + 1, board, realBoard, checked);
+
         List<Integer> uus7 = new ArrayList<>();
         uus7.add(x + 1);
-        uus7.add(y);
-        if (numbers[5] == 0 && !checked.contains(uus7)) openAround(x + 1, y, board, realBoard, checked);
+        uus7.add(y-1);
+        if (numbers[5] == 0 && !checked.contains(uus7)) openAround(x + 1, y-1, board, realBoard, checked);
+
         List<Integer> uus8 = new ArrayList<>();
         uus8.add(x + 1);
-        uus8.add(y - 1);
-        if (numbers[6] == 0 && !checked.contains(uus8)) openAround(x + 1, y - 1, board, realBoard, checked);
+        uus8.add(y);
+        if (numbers[6] == 0 && !checked.contains(uus8)) openAround(x + 1, y, board, realBoard, checked);
+
         List<Integer> uus9 = new ArrayList<>();
-        uus9.add(x);
-        uus9.add(y - 1);
-        if (numbers[7] == 0 && !checked.contains(uus9)) openAround(x, y - 1, board, realBoard, checked);
-
-
+        uus9.add(x+1);
+        uus9.add(y +1);
+        if (numbers[7] == 0 && !checked.contains(uus9)) openAround(x+1, y + 1, board, realBoard, checked);
     }
 }
