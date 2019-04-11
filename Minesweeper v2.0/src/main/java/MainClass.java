6fee3739ed9880aff99f25f977b8fde503e32e86
Eva Ibrus <eva.ibrus@hotmail.com>
2019-04-11 12:37:19 +0300

    Started with version 2.0

---
+++ Minesweeper v2.0/src/main/java/MainClass.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainClass extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("tere");
+    }
+}
